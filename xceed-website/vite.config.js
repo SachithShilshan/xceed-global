c74bb538f42f295634fb9d8b74e3b2683b57d814
SachithShilshan <sachith.edu6021@gmail.com>
2025-11-22 07:40:33 +0530

    Nov 22

--- xceed-website/vite.config.js
+++ xceed-website/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/xceed-global/'    // <- set this to /<repo-name>/
+  base: '/'    // <- set this to /<repo-name>/
 });
